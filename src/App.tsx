459de3bda06817c7545603faad9f6c4e26880507
Nelson de Melo Castanha Neto <nelsoncastanha@MacBook-de-Nelson.local>
2022-01-31 23:18:27 -0300

    part 1

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet"
+
+function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ]);
+
+  function createTweet() {
+    setTweets([...tweets, 'Tweet 5'])
+  }
+
+
+
+
+
+
+  return (
+    <div>
+      {tweets.map(tweet => {
+        return <Tweet text={tweet} />
+      })}
+
+      <button onClick={createTweet}>Adicionar tweet</button>
+    </div>
+  );
+}
+
+export default App
